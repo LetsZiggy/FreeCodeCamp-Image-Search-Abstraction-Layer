4c8b9fb598cd1518d6e82c188741b2f92ba98662
Glitch (letsziggy-freecodecamp-image-search-abstraction-layer) <none>
2018-01-08 18:07:55 +0000

    ⛱🕍 Updated with Glitch

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const https = require('https');
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 const mongo = require('mongodb').MongoClient;
 
 let dbURL = `mongodb://${process.env.DBUSER}:${process.env.DBPASSWORD}@ds245687.mlab.com:45687/freecodecamp`;
 let google = `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLEKEY}&cx=${process.env.GOOGLEID}&searchType=image`;
+let html = null;
+let css = null;
+
+fs.readFile('./www/index.html', (err, data) => {
+  if(err) { console.log(err); throw err; }
+  else {
+    html = data;
+  }
+});
+
+fs.readFile('./www/style.css', (err, data) => {
+  if(err) { console.log(err); throw err; }
+  else {
+    css = data;
+  }
+});
 
 function getSearch(search, offset, resHttp) {
   mongo.connect(dbURL, (err, client) => {
@@ -85,12 +102,16 @@ function determineQuery(query, res) {
 let server = http.createServer((req, res) => {
   req.url = req.url.slice(1);
   if(req.url === '') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end();
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(html);
   }
-  else if(req.url === 'favicon.ico') {
+  else if(req.url.includes('style.css')) {
+    res.writeHead(200, { 'Content-Type': 'text/css' });
+    res.end(css);
+  }
+  else if(req.url.includes('favicon.ico')) {
     res.writeHead(200, { 'Content-Type': 'image/x-icon' });
-    res.end('https://cdn.glitch.com/df0642e9-fb3f-4138-a464-3fa9b1a9b420%2Fblog_logo.ico?1515316087927');
+    res.end('https://cdn.glitch.com/22aed6df-466d-4435-bab7-885f082a7563%2Fblog_logo.ico?1515317128270');
   }
   else {
     determineQuery(req.url, res);
